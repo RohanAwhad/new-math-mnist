7a943bf5a747d55a79ada0ae51e8539402aa9861
Rohan Awhad <rawhad@redhat.com>
2026-03-06 10:23:43 -0500

    updated system prompt

--- prompts.py
+++ prompts.py
@@ -6,15 +6,6 @@ PROMPT_VERSION = "v2"
 
 SYSTEM_PROMPT = """You are evaluating expressions in a synthetic arithmetic system.
 
-Rules:
-1) a + b = a + b
-2) a - b = a - b
-3) a * b = a * b
-4) a / b = floor(a / b)
-5) a ## b = abs(a - b)
-6) a @@ b = max(a, b)
-7) a $$ b = min(a, b)
-
 Evaluation order: strictly left-to-right for all operators.
 Do not apply precedence. No parentheses.
 
